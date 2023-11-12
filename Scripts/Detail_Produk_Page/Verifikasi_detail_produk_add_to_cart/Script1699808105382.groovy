dc3b53b6959c93b3c82dac137063e953f77b4d02
New Amigos <New Amigos@DESKTOP-CPSCKDQ>
2023-11-13 00:12:35 +0700

    done

--- Scripts/Detail_Produk_Page/Verifikasi_detail_produk_add_to_cart/Script1699808105382.groovy
+++ Scripts/Detail_Produk_Page/Verifikasi_detail_produk_add_to_cart/Script1699808105382.groovy
@@ -17,6 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+'\''
 WebUI.verifyElementText(findTestObject('Detail_Produk_Page/label_swagLabs'), 'Swag Labs')
 
 WebUI.click(findTestObject('Detail_Produk_Page/Label_produkBikeLight'))
